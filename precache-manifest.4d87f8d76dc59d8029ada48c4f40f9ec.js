61998ddd18b81854918149026e81cbc3d4202ae7
frootas <frootas234@gmail.com>
2020-05-18 16:51:31 +1000

    Updates

---
+++ precache-manifest.4d87f8d76dc59d8029ada48c4f40f9ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74468a8556a8468e550f7b2f6468ddfd",
+    "url": "/multi-page-app/index.html"
+  },
+  {
+    "revision": "beab9d6e03468893e8c0",
+    "url": "/multi-page-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6b86313aa1b9cf3b03d4",
+    "url": "/multi-page-app/static/js/2.92bfe0d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/multi-page-app/static/js/2.92bfe0d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beab9d6e03468893e8c0",
+    "url": "/multi-page-app/static/js/main.6e3f806b.chunk.js"
+  },
+  {
+    "revision": "83195ba7fde24ea8b8a8",
+    "url": "/multi-page-app/static/js/runtime-main.285370a7.js"
+  }
+]);
